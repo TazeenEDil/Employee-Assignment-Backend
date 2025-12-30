1c8456c22dd715e3fb7d8ed5f2a03ac264b21467
TazeenEDil <tazeenedil470@gmail.com>
2025-12-30 23:23:38 +0500

    cleaned code

---
+++ Employee-Assignment/DTOs/UpdateEmployeeDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employee_Assignment.DTOs
+{
+    public class UpdateEmployeeDto
+    {
+            [Required(ErrorMessage = "Name is required")]
+            [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
+            public string Name { get; set; }
+
+            [Required(ErrorMessage = "Email is required")]
+            [EmailAddress(ErrorMessage = "Invalid email format")]
+            [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "Position is required")]
+            [StringLength(100, ErrorMessage = "Position cannot exceed 100 characters")]
+            public string Position { get; set; }
+    }
+}
+
